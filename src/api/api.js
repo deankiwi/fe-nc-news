23b9a8f85e58ee3793cdef2af5eae6bf6262a09f
Dean <dean.w1993@gmail.com>
2024-02-06 16:20:35 +0000

    feat: add error handling
    updating to capture error handling
    https://github.com/deanlicious/fe-nc-news/pull/1#discussion_r1479688381

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,12 @@
 import axios from "axios";
-// import sampleArticleData from "./sampleData/articleSampleData.json";
 
 export function getArticles() {
   return axios("https://nc-news-server-ccze.onrender.com/api/articles")
-  .then((response) => {
-
-    return response.data.articles
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .then((response) => {
+      return response.data.articles;
+    })
+    .catch((error) => {
+      console.log(error);
+      return Promise.reject(error);
+    });
 }
-
-
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -3,22 +3,35 @@ import { getArticles } from "../api/api";
 import { ArticleSummary } from "./ArticleSummary";
 import { Col, Row } from "react-bootstrap";
 import { Loading } from "./Loading";
+import { ErrorComponent } from "./ErrorComponent";
 
 //TODO Check articles are coming from newest first
+//TODO Display what filters the user has
 
 export function NewsList() {
   const [articles, setArticles] = useState([]);
   const [isLoadingArticles, setIsLoadingArticles] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    getArticles().then((articlesFetched) => {
-      setArticles(articlesFetched);
-      setIsLoadingArticles(false);
-    });
+    getArticles()
+      .then((articlesFetched) => {
+        setArticles(articlesFetched);
+        setIsLoadingArticles(false);
+      })
+      .catch((err) => {
+        console.log(err, "<--- in catch");
+        setIsLoadingArticles(false);
+        setError(err);
+      });
   }, []);
+  if (error) {
+    return <ErrorComponent error={error} />;
+  }
+
   return (
     <article>
+      <h2>Articles</h2>
       <Loading isLoading={isLoadingArticles} loadingDescription="articles" />
-      <h2> Articles Placeholder</h2>
       <Row xs={1} sm={2} lg={3} className="g-4">
         {articles.map((article) => (
           <Col key={article.article_id}>
